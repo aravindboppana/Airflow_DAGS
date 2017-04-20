19c5c9f1becf3ade957e7cdc996562a7457a8607
aravindboppana <aravind.boppana@clairvoyantsoft.com>
2017-04-20 09:23:16 -0700

    Update practical_exercise_dag.py

--- practical_exercise_dag.py
+++ practical_exercise_dag.py
@@ -113,7 +113,10 @@ load_user_total = BashOperator(
 
 task_id='load_user_total',
 
-bash_command="""sh /home/cloudera/airflow/dags/user_total.sh """,
+bash_command="""max_value=$(hive -e "select CASE WHEN max(total_users) is null THEN '0' ELSE max(total_users) END from hive.user_total")
+set -- $max_value
+value=$1
+hive -e "INSERT INTO table hive.user_total select current_timestamp as time_ran,count(id) as total_users,count(id)-$value as users_added from hive.user"  """,
 
 dag=dag)
 
